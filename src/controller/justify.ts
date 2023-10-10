170c7f9db1694a4c76add5dd59919609d2616dd3
Pablo Aldana <pablo.aldan@uc.cl>
2023-10-10 14:39:50 -0300

    doc justify controller

--- src/controller/justify.ts
+++ src/controller/justify.ts
@@ -1,17 +1,14 @@
-function justifyText(plainText: string): string {
-  const lines: string[] = plainText.split('\n');
-  const newLines: string[] = [];
-  lines.forEach((line) => {
-    newLines.push(justifyLine(line))
-  })
-  return newLines.join('\n');
-}
+const MAX_CHAR_LINE = 80;
 
-function justifyLine(plainText: string): string {
-  const MAX_CHAR_LINE = 80;
-  const words = plainText.split(' ');
+/**
+ * Justifies the given plain line by adding spaces between words to make
+ * a maximum length of 80 characters.
+ * @param plainLine - The plain text to justify.
+ * @returns The justified text.
+ */
+function justifyLine(plainLine: string): string {
+  const words = plainLine.split(' ');
   const lines: string[] = [];
-
   let currentLine = '';
 
   words.forEach((word) => {
@@ -24,30 +21,42 @@ function justifyLine(plainText: string): string {
   });
   lines.push(currentLine.trim());
 
-  // Add space for each line if line length < MAX_CHAR_LINE
   const justifiedLines: string[] = [];
   lines.forEach((line : string, key) => {
-    if (key == lines.length-1) {
-      justifiedLines.push(line)
-    }
-    else if (line.length < MAX_CHAR_LINE) {
-      let space_to_add = MAX_CHAR_LINE - line.length
-      let words = line.split(" ")
-      let word_to_add = 0
+    if (key == lines.length - 1) {
+      justifiedLines.push(line);
+    } else if (line.length < MAX_CHAR_LINE) {
+      let space_to_add = MAX_CHAR_LINE - line.length;
+      const words = line.split(' ');
+      let word_to_add = 0;
       while (space_to_add > 0) {
-        words[word_to_add] = words[word_to_add] + " "
-        space_to_add -= 1
-        word_to_add += 1
+        words[word_to_add] = `${words[word_to_add]} `;
+        space_to_add -= 1;
+        word_to_add += 1;
         if (word_to_add > words.length - 1) {
-          word_to_add = 0
+          word_to_add = 0;
         }
       }
-      justifiedLines.push(words.join(" "))
+      justifiedLines.push(words.join(' '));
     } else {
       justifiedLines.push(line);
     }
   });
   return justifiedLines.join('\n');
+}
+
+/**
+ * Takes in a plain text and justifies each line the same length.
+ * @param plainText - The input plain text to be justified
+ * @returns The justified text with each line having the same length
+ */
+function justifyText(plainText: string): string {
+  const lines: string[] = plainText.split('\n');
+  const newLines: string[] = [];
+  lines.forEach((line) => {
+    newLines.push(justifyLine(line));
+  });
+  return newLines.join('\n');
 }
 
 module.exports = justifyText;
